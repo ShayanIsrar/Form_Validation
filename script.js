752ed8a32e536ab3079ea90e70d647f1792eb93c
ShayanIsrar <shayanisrar19@gmail.com>
2024-08-30 11:18:18 -0700

    this is simple form validation

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('cpassword');
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    validate();
+})
+
+const validate = ()=>{
+    const username = username.value.trim();
+    const email = email.value.trim();
+    const phone = phone.value.trim();
+    const password = password.value.trim();
+    const cpassword = cpassword.value.trim();
+}
